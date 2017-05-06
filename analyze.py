986a57ef21ddd2c0a478ca5910b4d1d81f4c69b7
Al Momin <al.momin@outlook.com>
2017-05-06 16:13:12 -0400

    Add script to generate random data for comparison.

--- analyze.py
+++ analyze.py
@@ -1,16 +1,24 @@
 from scipy import stats
+from termcolor import colored
 import numpy as np
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-s", "--start", dest="start_year", type='int', default=1950,
+                  help="Start year for slope window", metavar="START")
+parser.add_option("-e", "--end", dest="end_year", type='int', default=2016,
+                  help="End year for slope window", metavar="END")
+parser.add_option("-w", "--window", type='int', dest="window", default=None,
+                  help="Specify a window")
+
+(options, args) = parser.parse_args()
+# Turn options Values into dictionary.
+options = vars(options)
+print options, args
 
 my_data = np.genfromtxt('data/GLB.Ts+dSST.csv', delimiter=',', skip_header=2)
 
 base_year = 1880
-start_year = 1950
-end_year =   2016
-
-years = np.array(range(start_year - base_year,end_year - base_year + 1))
-# I think 13 is the annual average column...
-annual_averaged_anomoly = my_data[start_year - base_year:end_year - base_year + 1][:,13]
-
 
 # Basically copied from the second answer here:
 # https://stackoverflow.com/questions/36400419/how-to-calculate-the-99-confidence-interval-for-the-slope-in-a-linear-regressio
@@ -21,11 +29,27 @@ def calc_ci(x, y, n_stds):
     return confidence_interval
 
 if __name__=='__main__':
-    # n is not very large here, say roughly n ~ 50
-    # But n_stds=2 still gives a roughly a 95% confidence interval so
-    # good enough for our purposes.
-    # FWIW n_std = 3 would still easily exclude 0.
-    n_stds = 2
-    ci = calc_ci(years, annual_averaged_anomoly, n_stds)
-    print "Slope: ", (ci[0] + ci[1]) / 2
-    print "CI: ", ci
+    start_year = int(options['start_year'])
+    end_year = int(options['end_year'])
+    window = end_year - start_year
+    if options['window'] != None and options['window'] < end_year - start_year:
+        window = options['window']        
+        
+
+    for i in range(start_year, end_year - window + 1):
+        j = i + window
+
+        years = np.array(range(i - base_year,j - base_year + 1))
+        # I think 13 is the annual average column...
+        annual_averaged_anomoly = my_data[i - base_year:j - base_year + 1][:,13]
+        # n is not very large here, say roughly n ~ 50
+        # But n_stds=2 still gives a roughly a 95% confidence interval so
+        # good enough for our purposes.
+        # FWIW n_std = 3 would still easily exclude 0.
+        n_stds = 2
+        ci = calc_ci(years, annual_averaged_anomoly, n_stds)
+
+        print "Years: %d to %d" % (i, j)
+        print "\tSlope:", colored("%.02f" % (100*(ci[0] + ci[1]) / 2), 'red'), \
+              "degrees C per century"
+        print "\tCI: [%.02f C/100y, %.02f C/100y]" % (100*ci[0], 100*ci[1])
---
+++ random-series.py
@@ -0,0 +1,30 @@
+from scipy import stats
+import numpy as np
+
+def calc_ci(x, y, n_stds):
+    slope,intercept,r_value,p_value,std_err = stats.linregress(x,y)
+    confidence_interval = [slope-n_stds*std_err,slope+n_stds*std_err]
+    return confidence_interval
+
+N = 100
+c = 0.0
+
+s = 1880
+base_temp = 0.0
+dCpC = 0.3
+l = 40
+noise = 0.9
+
+for i in range(0,N):
+    xs = np.array(range(s, s+l))
+    ys = np.array([base_temp - noise/2] * l) + \
+         (dCpC / 100.0) * np.array(range(0, l)) + \
+         noise * np.random.rand(1, l)
+
+    ci = calc_ci(xs, ys, 2)
+    if ci[0] < 0 and ci[1] > 0:
+        c += 1
+
+print "Increase by %.02f degC/Cent, %d year intervals %d times, noise=%.02f" \
+      % (dCpC, l, N, noise)
+print "Fraction of times 0 is in the CI:", c / N
